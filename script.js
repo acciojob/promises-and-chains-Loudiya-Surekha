1d118d2b7016a1a28900eefd7d7ed90e31a2349d
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-26 12:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+  function abc(event) {
+      event.preventDefault();
+
+      const name = document.getElementById("name").value.trim();
+      const age = document.getElementById("age").value.trim();
+      if (name === "" || age === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+      const checkAge = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (parseInt(age) > 18) {
+            resolve(name);
+          } else {
+            reject(name);
+          }
+        }, 4000);
+      });
+      checkAge
+        .then((userName) => {
+          alert(`Welcome, ${userName}. You can vote.`);
+        })
+        .catch((userName) => {
+          alert(`Oh sorry ${userName}. You aren't old enough.`);
+        });
+    }
